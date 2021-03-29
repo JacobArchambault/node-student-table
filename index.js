0f2c1df6797351eba67317bcd109db8784eb26ea
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-29 10:16:31 -0400

    Simplify

--- index.js
+++ index.js
@@ -9,31 +9,21 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));  // this allows us to get our screem image
 
-var scores = [                        // an array of objects
-   { name: "Alice", midterm: 98, final: 92 },
-   { name: "Bruce", midterm: 88, final: 82 },
-   { name: "Clara", midterm: 78, final: 72 },
-   { name: "David", midterm: 78, final: 77 },
-   { name: "Eldon", midterm: 82, final: 87 }
-];
+app.get('/students', (_req, res) => res.render('students', {
+   classData: [
+      { name: "Alice", midterm: 98, final: 92 },
+      { name: "Bruce", midterm: 88, final: 82 },
+      { name: "Clara", midterm: 78, final: 72 },
+      { name: "David", midterm: 78, final: 77 },
+      { name: "Eldon", midterm: 82, final: 87 }
+   ]
+}));
 
+app.get('/', (_req, res) => res.render('hello', { name: "Joan" }));
 
-app.get('/students', function (req, res) {
-   res.render('students', { classData: scores });
-});
+app.use((_req, res) => res.status(404).send("Sorry, no such page!"));
 
-app.get('/', function (req, res) {
-   res.render('hello', { name: "Joan" });
-});
-
-
-app.use(function (req, res) {
-   res.status(404).send("Sorry, no such page!")
-});
-
-app.listen(3000, function () {
-   console.log('App started on http://localhost:3000,  press Ctrl-C to terminate.');
-});
+app.listen(3000, console.log('App started on http://localhost:3000,  press Ctrl-C to terminate.'));
 
 
 
