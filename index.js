a08c83481de9e0071c878e4f68f33417fa6f6ea1
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-29 12:24:02 -0400

    npm update

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-// the app
 import express from 'express';
 import exphbs from 'express-handlebars';
 var app = express();
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-
 app.use(express.static('public'));  // this allows us to get our scream image
-
 app.get('/students', (_req, res) => res.render('students', {
    classData: [
       { name: "Alice", midterm: 98, final: 92 },
@@ -17,13 +13,6 @@ app.get('/students', (_req, res) => res.render('students', {
       { name: "Eldon", midterm: 82, final: 87 }
    ]
 }));
-
 app.get('/', (_req, res) => res.render('hello', { name: "Jacob" }));
-
 app.use((_req, res) => res.status(404).send("Sorry, no such page!"));
-
 app.listen(3000, console.log('App started on http://localhost:3000,  press Ctrl-C to terminate.'));
-
-
-
-
