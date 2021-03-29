2456d29979c0bff33e04d6951549c06a5bbeb743
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-29 12:20:40 -0400

    Use es6 imports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // the app
-var express = require('express');
-var exphbs = require('express-handlebars');
+import express from 'express';
+import exphbs from 'express-handlebars';
 var app = express();
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
