c74e03ea445e21fb311f690832aeff9171f0661f
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-29 10:00:03 -0400

    Migrate readbeforerunning.txt contents into README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # node-student-table
 A basic node.js application displaying a table of student names and grades.
+
+
+Before running this program, run the following from the command prompt after navigating to the program folder:
+
+>npm init -y
+>npm install express --save
+>npm install express-handlebars --save
--- index.js
+++ index.js
@@ -3,36 +3,36 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-var exphbs = require('express-handlebars'); 
-app.engine('handlebars', exphbs({defaultLayout: 'main'})); 
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));  // this allows us to get our screem image
 
 var scores = [                        // an array of objects
-   {name:"Alice", midterm: 98, final: 92},
-   {name:"Bruce", midterm: 88, final: 82},
-   {name:"Clara", midterm: 78, final: 72},
-   {name:"David", midterm: 78, final: 77},
-   {name:"Eldon", midterm: 82, final: 87}
+   { name: "Alice", midterm: 98, final: 92 },
+   { name: "Bruce", midterm: 88, final: 82 },
+   { name: "Clara", midterm: 78, final: 72 },
+   { name: "David", midterm: 78, final: 77 },
+   { name: "Eldon", midterm: 82, final: 87 }
 ];
 
 
-app.get('/students', function(req, res) {
-   res.render('students', {classData: scores});  
+app.get('/students', function (req, res) {
+   res.render('students', { classData: scores });
 });
 
-app.get('/', function(req, res) {
-   res.render('hello', {name: "Joan"});
+app.get('/', function (req, res) {
+   res.render('hello', { name: "Joan" });
 });
 
 
 app.use(function (req, res) {
-  res.status(404).send("Sorry, no such page!")
+   res.status(404).send("Sorry, no such page!")
 });
 
-app.listen(3000,  function () {
-   console.log('App started on http://localhost:3000,  press Ctrl-C to terminate.' );
+app.listen(3000, function () {
+   console.log('App started on http://localhost:3000,  press Ctrl-C to terminate.');
 });
 
 
